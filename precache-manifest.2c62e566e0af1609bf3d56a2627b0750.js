9078e3a76c618ddd87b7c7e0c3acda386b8066bd
Kanchan Nannavare <kanchannannavare@Apples-MacBook-Pro.local>
2019-11-12 14:21:00 +0530

    Updates

---
+++ precache-manifest.2c62e566e0af1609bf3d56a2627b0750.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a68c9719308e1506294cb67e594c1695",
+    "url": "/Kanchan_portfolio/index.html"
+  },
+  {
+    "revision": "4711faca044887516242",
+    "url": "/Kanchan_portfolio/static/css/main.b9e2ac8f.chunk.css"
+  },
+  {
+    "revision": "68760745230cdd869900",
+    "url": "/Kanchan_portfolio/static/js/2.dbe60e30.chunk.js"
+  },
+  {
+    "revision": "4711faca044887516242",
+    "url": "/Kanchan_portfolio/static/js/main.00218cce.chunk.js"
+  },
+  {
+    "revision": "b752688d51a3353a1f91",
+    "url": "/Kanchan_portfolio/static/js/runtime-main.023fa34f.js"
+  },
+  {
+    "revision": "87782d294f2f151c3e1c6bf1d89dbaa4",
+    "url": "/Kanchan_portfolio/static/media/Clouds.87782d29.JPG"
+  },
+  {
+    "revision": "9db49b2c42770317a1ca9c1474d5d243",
+    "url": "/Kanchan_portfolio/static/media/earthquake.9db49b2c.JPG"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Kanchan_portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "049768a0b8f3094f38ac70f927cb3227",
+    "url": "/Kanchan_portfolio/static/media/paanini.049768a0.png"
+  },
+  {
+    "revision": "f598f59031f9375693bf7290ed3eeca9",
+    "url": "/Kanchan_portfolio/static/media/resume.f598f590.pdf"
+  },
+  {
+    "revision": "5ebe3c16d22952aa4c0b471e4df8da10",
+    "url": "/Kanchan_portfolio/static/media/steel.5ebe3c16.png"
+  }
+]);
